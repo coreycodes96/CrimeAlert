9a6a61975705c85a0d52a4975ecfee0d28d86e90
unknown <coreycodes@outlook.com>
2021-04-21 16:40:11 +0100

    new project

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,5 +7,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController{
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;}
+use Illuminate\Auth;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,6 +26,8 @@ class HomeController extends Controller
         return view('home');
     }
 
-    public function store(){
-        return 'hello';}
+    public function store()
+    {
+        return 'bye';
+    }
 }
